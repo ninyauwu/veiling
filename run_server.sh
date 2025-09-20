88caecc13ccabe6deaec143a43b3db255e8a1c40
ninyauwu <alexblaauw@xs4all.nl>
2025-09-20 15:58:43 +0200

    Add docker build command to run_server.sh
    - Added aforementioned build command
    - Cleaned up container creation
    - Streamlined dockerfile to hopefully not reset cache
    each time run_server.sh changes

--- run_server.sh
+++ run_server.sh
@@ -1,1 +1,3 @@
-docker run -p 32274:8080 -t veilingserver
+docker build -f Veiling.Server/Dockerfile -t veilingserver .
+docker rm veiling &>/dev/null || true
+docker run --name veiling -p 32274:8080 -t veilingserver
