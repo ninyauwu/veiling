8c992fb8340bc7dd1d92c1c98a3bfcd0d144913f
NinyaUwU <alexblaauw@xs4all.nl>
2025-10-01 14:25:19 +0200

    Fix initialize_env.py bugs
    - Fixed certificate creation forgetting to ensure the directory
    exists before attempting to run the command.
    - Fixed database password not being set if local connection fails.
    - Dried up some wet code

--- initialize_env.py
+++ initialize_env.py
@@ -16,8 +16,27 @@ def can_connect(host, port=1433, timeout=4):
         return False
 
 def create_certificate(cert_file, password):
+    cert_dir = os.path.dirname(cert_file)
+    if cert_dir and not os.path.exists(cert_dir):
+        os.makedirs(cert_dir, exist_ok=True)
     result = subprocess.run(["dotnet", "dev-certs", "https", "-ep", cert_file, "-p", password], capture_output=True, text=True)
     print (result.stdout)
+    
+def certificate_setup(cert_dir, cert_file):
+    password = getpass.getpass("Enter a password for your (local) SSL certificate: ")
+    set_key(env_path, "CERT_PASSWORD", password, quote_mode="never")
+    create_certificate(cert_file, password)
+    set_key(env_path, "CERT_PATH", str(cert_dir), quote_mode="never")
+    
+def create_db_username():
+    print("Please enter your SQL system admin name (will be saved locally):", end = " ")
+    name = input()
+    set_key(env_path, "DB_USERNAME", name)
+    
+def create_db_password():
+    password = getpass.getpass("Please enter your SQL system admin password (will be saved locally): ")
+    set_key(env_path, "DB_PASSWORD", password)
+    
 
 # Setup path vars
 is_windows = platform.system() == "Windows"
@@ -33,16 +52,10 @@ env_vars = dotenv_values(env_path)
 print("\n==Setting up SSL certificate==")
 if cert_file.exists() & ("CERT_PASSWORD" in env_vars) & ("CERT_PATH" in env_vars):
     if not click.confirm("Certificate already exists. Skip certificate creation?"):
-        password = getpass.getpass("Enter a password for your (local) SSL certificate: ")
-        set_key(env_path, "CERT_PASSWORD", password, quote_mode="never")
-        create_certificate(cert_file, password)
-        set_key(env_path, "CERT_PATH", str(cert_dir), quote_mode="never")
+        certificate_setup(cert_dir, cert_file)
 else:
     print(f"No certificate found, creating SSL certificate at {cert_dir}")
-    password = getpass.getpass("Enter a password for your (local) SSL certificate: ")
-    set_key(env_path, "CERT_PASSWORD", password, quote_mode="never")
-    create_certificate(cert_file, password)
-    set_key(env_path, "CERT_PATH", str(cert_dir), quote_mode="never")
+    certificate_setup(cert_dir, cert_file)
 
 # Connect to database
 print("\n==Setting up database connection==")
@@ -65,18 +78,19 @@ if connection_string == None:
         print("Successfully connected to localhost SQL server")
     else:
         print("Failed to connect to localhost SQL server.")
-        print("Contact Nina, your SQL server might not be configured properly.")
+        print("That's ok, it just means you can't run the server containerless.")
+        print("Contact Nina to configure your local SQL database if you do want to run without docker.")
+        if not ("DB_PASSWORD" in env_vars):
+            print("Nevertheless, we need a system admin password for the container database.")
+            create_db_password()
 
 if connection_string:
     set_key(env_path, "DB_SERVER", f"tcp:{connection_string},1433")
     set_key(env_path, "DB_NAME", "master")
     print()
-    print("Please enter your SQL system admin name (will be saved locally):", end = " ")
-    name = input()
-    password = getpass.getpass("Please enter your SQL system admin password (will be saved locally): ")
-
-    set_key(env_path, "DB_USERNAME", name)
-    set_key(env_path, "DB_PASSWORD", password)
+    create_db_username()
+    create_db_password()
 
 print(f".env file updated at {env_path.resolve()}")
+print("You should be able to run the project with 'docker compose up --build' now :D")
 
