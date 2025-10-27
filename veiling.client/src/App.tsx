c6a82303ee9994d57de017bbb825e1aea63e4ff6
Stefaan Molenaar <StefaanLMolenaar@gmail.com>
2025-10-27 10:35:43 +0100

    Add location dropdown menu component, tsx & css files.
    - lil' animation for dropdown arrow
    - 3 locations; Rdam, Adam, Delft

--- veiling.client/src/App.tsx
+++ veiling.client/src/App.tsx
@@ -1,58 +1,27 @@
-import { useEffect, useState } from 'react';
+// src/App.tsx
+import { useState } from 'react';
 import './App.css';
-
-interface Forecast {
-    date: string;
-    temperatureC: number;
-    temperatureF: number;
-    summary: string;
-}
+import LocationDropdown from './components/location_dropdown.tsx';
 
 function App() {
-    const [forecasts, setForecasts] = useState<Forecast[]>();
-
-    useEffect(() => {
-        populateWeatherData();
-    }, []);
-
-    const contents = forecasts === undefined
-        ? <p><em>Loading... Please refresh once the ASP.NET backend has started. See <a href="https://aka.ms/jspsintegrationreact">https://aka.ms/jspsintegrationreact</a> for more details.</em></p>
-        : <table className="table table-striped" aria-labelledby="tableLabel">
-            <thead>
-                <tr>
-                    <th>Date</th>
-                    <th>Temp. (C)</th>
-                    <th>Temp. (F)</th>
-                    <th>Summary</th>
-                </tr>
-            </thead>
-            <tbody>
-                {forecasts.map(forecast =>
-                    <tr key={forecast.date}>
-                        <td>{forecast.date}</td>
-                        <td>{forecast.temperatureC}</td>
-                        <td>{forecast.temperatureF}</td>
-                        <td>{forecast.summary}</td>
-                    </tr>
-                )}
-            </tbody>
-        </table>;
+    const [selectedLocation, setSelectedLocation] = useState('');
 
     return (
-        <div>
-            <h1 id="tableLabel">Weather forecast</h1>
-            <p>This component demonstrates fetching data from the server.</p>
-            {contents}
+        <div className="app">
+            <h1>Veiling Test</h1>
+
+            <LocationDropdown
+                value={selectedLocation}
+                onChange={setSelectedLocation}
+            />
+
+            {selectedLocation && (
+                <div className="result">
+                    <p>Je hebt gekozen: <strong>{selectedLocation}</strong></p>
+                </div>
+            )}
         </div>
     );
-
-    async function populateWeatherData() {
-        const response = await fetch('weatherforecast');
-        if (response.ok) {
-            const data = await response.json();
-            setForecasts(data);
-        }
-    }
 }
 
 export default App;
