97def1386ab3706c18dc08fb26d0a8a41e61022d
ninyauwu <alexblaauw@xs4all.nl>
2025-10-27 13:44:02 +0100

    Add simple reusable buttons
    Buttons exist in two forms. There are regular simple buttons and
    buttons with arrow SVGs. The latter can be expanded later to allow
    for any icon to be set. The two buttons also have an "appearance"
    property that allows you to define whether they have the full color
    primary appearance, or the bordered secondary appearance.
    
    The button size can also change depending on the text size.
    The buttons can be used anywhere in the website and should in most
    cases be the standard button form.

---
+++ veiling.client/src/components/SimpeleKnop.tsx
@@ -0,0 +1,72 @@
+import "./SimpeleKnop.css";
+import type { ReactNode } from "react";
+
+export default function SimpeleKnop({
+  children,
+  label,
+  onClick,
+  disabled,
+  type = "button",
+  appearance = "secondary",
+}: SimpeleKnopProps) {
+  return (
+    <button
+      className={"simpele-knop-" + appearance}
+      onClick={onClick}
+      disabled={disabled}
+      type={type}
+    >
+      {label}
+      {children}
+    </button>
+  );
+}
+
+export function SimpeleKnopPijl({
+  children,
+  direction,
+  label,
+  onClick,
+  disabled = false,
+  type = "button",
+  appearance = "secondary",
+}: SimpeleKnopPijlProps) {
+  return (
+    <button
+      className={"simpele-knop " + appearance}
+      onClick={onClick}
+      disabled={disabled}
+      type={type}
+    >
+      {direction == "left" ? getIcon("/icons/arrow_left.svg") : null}
+      {label}
+      {children}
+      {direction == "right" ? getIcon("/icons/arrow_right.svg") : null}
+    </button>
+  );
+}
+
+function getIcon(maskUrl: string) {
+  return (
+    <div
+      className="masked-color"
+      style={{
+        maskImage: `url(${maskUrl})`,
+        WebkitMaskImage: `url(${maskUrl})`,
+      }}
+    />
+  );
+}
+
+interface SimpeleKnopProps {
+  children: ReactNode;
+  label?: string;
+  onClick?: () => void;
+  disabled?: boolean;
+  type?: "button" | "submit" | "reset";
+  appearance?: "primary" | "secondary";
+}
+
+interface SimpeleKnopPijlProps extends SimpeleKnopProps {
+  direction: "left" | "right";
+}
