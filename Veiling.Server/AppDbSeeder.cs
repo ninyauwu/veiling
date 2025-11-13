f50e406e2531a46867072d8747c73501e12734ee
Stefaan Molenaar <StefaanLMolenaar@gmail.com>
2025-11-13 12:33:41 +0100

    Change seed data to something we can show in review.

--- Veiling.Server/AppDbSeeder.cs
+++ Veiling.Server/AppDbSeeder.cs
@@ -7,7 +7,7 @@ namespace Veiling.Server
             if (!context.Locaties.Any())
             {
                 context.Locaties.AddRange(
-                    new Models.Locatie { Naam = "Amsterdam", KlokId = 1, Actief = false },
+                    new Models.Locatie { Naam = "Amsterdam", KlokId = 1, Actief = true },
                     new Models.Locatie { Naam = "Rotterdam", KlokId = 2, Actief = false },
                     new Models.Locatie { Naam = "Delft", KlokId = 3, Actief = true }
                 );
@@ -16,7 +16,7 @@ namespace Veiling.Server
             {
                 // Update bestaande data
                 var amsterdam = context.Locaties.FirstOrDefault(l => l.Naam == "Amsterdam");
-                if (amsterdam != null) amsterdam.Actief = false;
+                if (amsterdam != null) amsterdam.Actief = true;
         
                 var rotterdam = context.Locaties.FirstOrDefault(l => l.Naam == "Rotterdam");
                 if (rotterdam != null) rotterdam.Actief = false;
