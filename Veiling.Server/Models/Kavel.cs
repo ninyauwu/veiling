16c615ac31d1f8495f24490d6df3e995395f8c6e
Omashu <teg12toy@gmail.com>
2025-12-08 13:11:36 +0100

    Remove comments

--- Veiling.Server/Models/Kavel.cs
+++ Veiling.Server/Models/Kavel.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public string Naam { get; set; } = string.Empty;
         public string Beschrijving { get; set; } = string.Empty;
-        public string ArtikelKenmerken { get; set; } = string.Empty; // wat wordt hiermee bedoeld
+        public string ArtikelKenmerken { get; set; } = string.Empty; 
         public int GekochteContainers { get; set; }
         public float MinimumPrijs { get; set; }
         public float MaximumPrijs { get; set; }
@@ -15,11 +15,11 @@
         public string Kavelkleur { get; set; } = string.Empty; 
         public int Karnummer { get; set; }
         public int Rijnummer { get; set; }
-        public int HoeveelheidContainers { get; set; } // moet erbij komen
-        public int AantalProductenPerContainer { get; set; } // moet erbij komen
-        public float LengteVanBloemen { get; set; } // moet erbij komen
-        public float GewichtVanBloemen { get; set; }  // moet erbij komen
-        public string StageOfMaturity { get; set; } = string.Empty; // moet erbij komen
+        public int HoeveelheidContainers { get; set; } 
+        public int AantalProductenPerContainer { get; set; }
+        public float LengteVanBloemen { get; set; } 
+        public float GewichtVanBloemen { get; set; } 
+        public string StageOfMaturity { get; set; } = string.Empty; 
         public char NgsCode { get; set; }
         public string Keurcode { get; set; } = string.Empty;
         public int Fustcode { get; set; }
