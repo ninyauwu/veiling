cf1fdd946de1afbdb4f23fe67cc375b06eeb9d57
Stefaan Molenaar <StefaanLMolenaar@gmail.com>
2026-01-08 12:21:06 +0100

    Edit register API call

--- veiling.client/src/components/RegistratieWidget.tsx
+++ veiling.client/src/components/RegistratieWidget.tsx
@@ -30,16 +30,13 @@ function Registratie() {
     }
 
     try {
-      const response = await fetch('/api/gebruikers/register', {
+      const response = await fetch('/api/register', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
           email,
-          firstName,
-          lastName,
-          phoneNumber,
           password
         }),
       });
