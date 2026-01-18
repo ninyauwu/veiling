b6e7a295e9f77319a99a62a049baa04367cd7d87
Stefaan Molenaar <StefaanLMolenaar@gmail.com>
2026-01-18 17:26:21 +0100

    Made Roles enum public.

--- Veiling.Server.Test/CustomWebApplicationFactory.cs
+++ Veiling.Server.Test/CustomWebApplicationFactory.cs
@@ -1,14 +1,19 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Veiling.Server;
+using Veiling.Server.Models;
 
 namespace Veiling.Server.Test
 {
     public class CustomWebApplicationFactory : WebApplicationFactory<Program>
     {
+        private static bool _rolesInitialized = false;
+        private static readonly object _lock = new object();
+
         protected override void ConfigureWebHost(IWebHostBuilder builder)
         {
             builder.UseEnvironment("Testing");
@@ -37,7 +42,30 @@ namespace Veiling.Server.Test
                 using var scope = sp.CreateScope();
                 var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
                 db.Database.EnsureCreated();
+                
+                // Initialize roles for testing - only once
+                lock (_lock)
+                {
+                    if (!_rolesInitialized)
+                    {
+                        var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+                        InitializeRoles(roleManager).Wait();
+                        _rolesInitialized = true;
+                    }
+                }
             });
+        }
+        
+        private async Task InitializeRoles(RoleManager<IdentityRole> roleManager)
+        {
+            // Create all roles from the Role enum
+            foreach (var roleName in Enum.GetNames(typeof(Role)))
+            {
+                if (!await roleManager.RoleExistsAsync(roleName))
+                {
+                    await roleManager.CreateAsync(new IdentityRole(roleName));
+                }
+            }
         }
     }
 
