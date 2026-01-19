f8edc38c4fca6f65bb37a888a9a5359724565bad
Omashu <teg12toy@gmail.com>
2026-01-19 22:30:30 +0100

    Update Loggedin

--- veiling.client/src/components/HeaderLoggedin.tsx
+++ veiling.client/src/components/HeaderLoggedin.tsx
@@ -1,27 +1,38 @@
-import React from "react";
-import type { Me } from "../types";
-
-type HeaderProps = {
-  me: Me | null;
+type LoggedInHeaderProps = {
+  email: string;
+  roles: string[];
 };
 
-const Header: React.FC<HeaderProps> = ({ me }) => {
+export default function LoggedInHeader({ email, roles }: LoggedInHeaderProps) {
   return (
-    <header style={{ padding: "16px", background: "#eee" }}>
-      {me ? (
-        <div>
-          <span>Welkom, {me.email}</span>
-          <span style={{ marginLeft: "16px" }}>
-            Rollen: {me.roles.join(", ")}
-          </span>
-        </div>
-      ) : (
-        <div>
-          <span>Niet ingelogd</span>
-        </div>
-      )}
+    <header
+      style={{
+        padding: "10px 20px",
+        backgroundColor: "#4caf50",
+        color: "#fff",
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+      }}
+    >
+      <div>
+        <span>Welkom, {email}</span>
+      </div>
+      <div>
+        <span>Rollen: {roles.join(", ")}</span>
+      </div>
+      <button
+        onClick={() => {
+          alert("Logout nog niet geïmplementeerd");
+        }}
+        style={{
+          marginLeft: "20px",
+          padding: "5px 10px",
+          cursor: "pointer",
+        }}
+      >
+        Uitloggen
+      </button>
     </header>
   );
-};
-
-export default Header;
+}
