a29700210169d18f26c97c4364badd9b77c028cf
Omashu <teg12toy@gmail.com>
2026-01-31 15:01:48 +0100

    Fix kavel kleur

--- veiling.client/src/components/KavelInfo.tsx
+++ veiling.client/src/components/KavelInfo.tsx
@@ -272,7 +272,7 @@ function KavelInfo({
                 value: (
                   <div
                     style={{
-                      backgroundColor: "#" + kavel.kavelkleur,
+                      backgroundColor: kavel.kavelkleur,
                       width: "24px",
                       height: "24px",
                     }}
